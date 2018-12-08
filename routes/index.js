99e51f773f8adce82498b63af79ffe5abe7a376f
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2018-12-08 14:48:27 +0100

    cleaned and using VSCode for live preview

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-// http://localhost:7000/
-router.get('/', function(req, res, next) {
-    res.render('pages/index');
-});
-
-module.exports = router;
