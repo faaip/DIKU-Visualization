2dee3e196c71b0f9733d12abffb97ab103ddee05
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2019-01-11 14:42:13 +0100

    added slider

--- assignment_5/js/side_controls.js
+++ assignment_5/js/side_controls.js
@@ -1,11 +1,12 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("sideControls").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("sideButton").style.opacity = 0;
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("sideControls").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.getElementById("sideButton").style.opacity = 1;
 }
+
