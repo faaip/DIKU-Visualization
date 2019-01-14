a734dc6c65c381d200d627a91c0c4793a6b5e469
KasperDjernaes <Kasperdjernaeslarsen1@gmail.com>
2019-01-14 18:58:54 +0100

    Added activity, year and month to the geojson

--- assignment_5/preprocessing/data_preprocessing.py
+++ assignment_5/preprocessing/data_preprocessing.py
@@ -23,7 +23,51 @@ file_to_open = data_folder / data_name
 
 with open(file_to_open, 'r') as read_file:
     data = json.load(read_file)
-    
+
+# Decide how many points should be used (Default all points)
+
+#number_of_points = len(data['locations'])
+number_of_points = 1000
+
+# Get all activities as a list. If no activity is present give it a NA value
+
+activities = []
+for i in range(len(data['locations'])):
+    if 'activity' in data['locations'][i].keys():
+        act = data['locations'][i]['activity'][0]['activity'][0]['type']
+        activities.append(act)
+    else:
+        activities.append('NA')
+
+# Define a function that takes a timestamp in Ms and returns year/month
+
+def year_maker(time_stamp, date_type='Year'):
+
+    if date_type == 'Year':
+        timestampS = round(int(time_stamp) / 1000)
+        date = datetime.fromtimestamp(timestampS)
+        date_value = date.year
+
+    elif date_type == 'Month':
+        timestampS = round(int(time_stamp) / 1000)
+        date = datetime.fromtimestamp(timestampS)
+        date_value = date.strftime("%B")
+
+    return date_value
+
+# get year as a lists
+years = [year_maker(x['timestampMs']) for x in data['locations']]
+
+# Maunally set some of them to 2018 for the test set
+years2 = [year if count % 2 else '2018' for count, year in enumerate(years)]
+
+# get months as a list
+months = [year_maker(x['timestampMs'], date_type='Month') for x in data['locations']]
+
+# Manually set some of the months to february for the test set to do filters
+
+months2 = [month if count % 2 else 'February' for count, month in enumerate(months)]
+
 '''Adding duration to each location point ''' 
 
 # Get all time stamps as a numpy array
@@ -50,17 +94,33 @@ mask = np.where(outlierBool) # Turn boolean into index of outliers
 
 diff[mask] = newMean # Replace outliers with the mean estimted without outliers
 
+# Create the zip object that contains the preocessed information that has to be put into the geojson
+# 0: dic with long and lat
+# 1: list with duration in sec
+# 2: activity type
+# 3: year
+# 4: month
+
+zip_object = zip(data['locations'][0:number_of_points], 
+        diff[0:number_of_points], 
+        activities[0:number_of_points], 
+        years2[0:number_of_points], 
+        months2[0:number_of_points])
+
 geojson = {
         "type": "FeatureCollection",
         "features":[
                 {
-                        "type":"Feature","properties":{"durationSec":d[1]}, 
+                        "type":"Feature","properties":{"durationSec":d[1],
+                        "activity": d[2],
+                        "year": d[3],
+                        "month": d[4]}, 
                                 "geometry":{"type":"Point",
                                 "coordinates": [round(d[0]["longitudeE7"] / 1e7,5), 
                                                 round(d[0]["latitudeE7"] / 1e7,5)],
                                 },
                                 
-                                } for d in zip(data['locations'],diff)] 
+                                } for d in zip_object] #Number of points is specified earlier
     }
     
 
@@ -69,11 +129,10 @@ geojson = {
 string = json.dumps(geojson, separators=(',', ':')) # Convert the geojson to a string for export purposes
 
 # Path to where you want to export the file
-export_folder = 'C:/Users/user/Desktop/IT and Cognition/Visualisation/finalProject/'
+export_folder = 'C:/Users/user/Desktop/IT and Cognition/Visualisation/finalProject/DIKIU_Visualization/assignment_5/data'
 export_path = Path(export_folder)
-export_file_name = 'placeringsoversigt.geojson'
+export_file_name = 'placeringsoversigt_small.geojson'
 export_path_name = export_path / export_file_name
-
 
 #Export the file as geojson to specified location
     
