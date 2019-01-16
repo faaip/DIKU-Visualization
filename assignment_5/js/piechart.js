28967486c8c539db2753fe7aad8ace90394794c6
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2019-01-16 20:56:25 +0100

    fixed pie chart loading error

--- assignment_5/js/piechart.js
+++ assignment_5/js/piechart.js
@@ -1,9 +1,6 @@
-function createPieChart(data) {
-    var text = "";
+function createPieChart(query) {
     var width = 200;
     var height = 200;
-    var thickness = 40;
-    var duration = 750;
     var padding = 10;
     var opacity = .8;
     var opacityHover = 1;
@@ -14,17 +11,24 @@ function createPieChart(data) {
     var color = d3.scaleOrdinal(d3.schemeCategory10);
 
     document.getElementById("piechart").innerHTML = "";
+    var data = getCounts(query);
 
-    if (data.length == 0) {
-        document.getElementById("piechart").innerHTML = "No data loaded :(";
-        return;
-    }
+    // if (data.length < 1) {
+    //     document.getElementById("piechart").innerHTML = "No data loaded :(";
+    //     return;
+    // }
 
     var total = 0;
 
-    data.forEach(element => {
-        total += element.value;
+    data = $.map(data, function (item, index) {
+        var checkBox = document.getElementById(item.name);
+        if (checkBox.checked == false) {
+            return null;
+        }
+        return item;
     });
+
+    console.log(data);
 
     var svg = d3.select("#piechart")
         .append('svg')
